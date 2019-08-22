cd08efe94e0136c7cb292e5a77c8ed02f64f5efa
Nilesh Rathi <nileshrathi01@gmail.com>
2019-08-22 11:18:03 +0530

    Upgrade ESLint (#1)
    * upgraded eslint
    
    * fix eslint errors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const aws = require('aws-sdk');
+
 const s3 = new aws.S3();
 const sharp = require('sharp');
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
 
-const handler = require('./index').handler;
+const { handler } = require('./index');
 
 // Create a server
 http.createServer((request, response) => {
@@ -21,9 +21,9 @@ http.createServer((request, response) => {
     // Read Lambda output format and send back a response
     // http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-set-up-simple-proxy.html#api-gateway-simple-proxy-for-lambda-output-format
     response.writeHead(responseDef.statusCode, responseDef.headers);
-    const decodedBody = responseDef.isBase64Encoded ?
-                        new Buffer(responseDef.body, 'base64') :
-                        responseDef.body;
+    const decodedBody = responseDef.isBase64Encoded
+      ? Buffer.from(responseDef.body, 'base64')
+      : responseDef.body;
     response.write(decodedBody);
     response.end();
   });
